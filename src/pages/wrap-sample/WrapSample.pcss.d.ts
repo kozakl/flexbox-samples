085743659669d8a9f90d7cef61eff71f14ba1ee2
Luke Kozak <kozakluke@gmail.com>
2018-11-20 09:59:56 +0100

    Refactor wrap-sample

--- src/pages/wrap-sample/WrapSample.pcss.d.ts
+++ src/pages/wrap-sample/WrapSample.pcss.d.ts
@@ -1,4 +1,4 @@
-export const flex:string;
+export const wrap:string;
 export const div1:string;
 export const div2:string;
 export const div3:string;
--- src/pages/wrap-sample/WrapSample.tsx
+++ src/pages/wrap-sample/WrapSample.tsx
@@ -4,7 +4,7 @@ import * as style from './WrapSample.pcss';
 export default function WrapSample()
 {
     return (
-        <div className={style.content}>
+        <div className={style.wrap}>
             <div className={style.div1}>DIV-1</div>
             <div className={style.div2}>DIV-2</div>
             <div className={style.div3}>DIV-3</div>

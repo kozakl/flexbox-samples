1015462a36c73b26eb85750df19edc69c5974313
Luke Kozak <kozakluke@gmail.com>
2018-11-20 09:31:17 +0100

    Clean flex-sample

--- src/pages/flex-sample/FlexSample.tsx
+++ src/pages/flex-sample/FlexSample.tsx
@@ -4,7 +4,7 @@ import * as style from './FlexSample.pcss';
 export default function FlexSample()
 {
     return (
-        <div className={style.content}>
+        <div className={style.flex}>
             <div className={style.div1}>DIV-1</div>
             <div className={style.div2}>DIV-2</div>
             <div className={style.div3}>DIV-3</div>

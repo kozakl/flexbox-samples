670aefda99f2796517ec62c646c4b3cc20e88657
Luke Kozak <kozakluke@gmail.com>
2018-11-20 09:47:38 +0100

    Create wrap-sample

--- src/components/content/Content.tsx
+++ src/components/content/Content.tsx
@@ -3,6 +3,7 @@ import {Redirect, Route, Switch} from 'react-router';
 import {HashRouter} from 'react-router-dom';
 import {FlexSample} from '../../pages/flex-sample';
 import {GrowSample} from '../../pages/grow-sample';
+import {WrapSample} from '../../pages/wrap-sample';
 
 export default function Content()
 {
@@ -16,11 +17,13 @@ export default function Content()
                         window.location.href = event.target.value}>
                     <option value="#/flex">Flex</option>
                     <option value="#/grow">Grow</option>
+                    <option value="#/wrap">Wrap</option>
                 </select>
                 <Switch>
                     <Redirect from="/" to='/flex' exact/>
                     <Route path="/flex" component={FlexSample}/>
                     <Route path="/grow" component={GrowSample}/>
+                    <Route path="/wrap" component={WrapSample}/>
                 </Switch>
             </>
         </HashRouter>
---
+++ src/pages/wrap-sample/WrapSample.pcss.d.ts
@@ -0,0 +1,5 @@
+export const flex:string;
+export const div1:string;
+export const div2:string;
+export const div3:string;
+export const div4:string;

55400ed19999fccd7f0acda73fe1355eecfb1f06
Luke Kozak <kozakluke@gmail.com>
2018-11-21 11:55:57 +0100

    Update and center copy in grow-sample

--- src/pages/grow-sample/GrowSample.pcss.d.ts
+++ src/pages/grow-sample/GrowSample.pcss.d.ts
@@ -1,5 +1,5 @@
 export const grow:string;
-export const div1:string;
-export const div2:string;
-export const div3:string;
-export const div4:string;
+export const element1:string;
+export const element2:string;
+export const element3:string;
+export const element4:string;
--- src/pages/grow-sample/GrowSample.tsx
+++ src/pages/grow-sample/GrowSample.tsx
@@ -5,10 +5,10 @@ export default function GrowSample()
 {
     return (
         <div className={style.grow}>
-            <div className={style.div1}>DIV-1</div>
-            <div className={style.div2}>DIV-2</div>
-            <div className={style.div3}>DIV-3</div>
-            <div className={style.div4}>DIV-4</div>
+            <div className={style.element1}>1</div>
+            <div className={style.element2}>2</div>
+            <div className={style.element3}>3</div>
+            <div className={style.element4}>4</div>
         </div>
     );
 }
